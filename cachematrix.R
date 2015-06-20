7a8d09be2ea444ed3742919697e37169ade3bdd2
Lorenzo Stanton <renzxl@gmail.com>
2015-06-19 20:57:25 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachMatrix will check if a matrix is stored in cache and return it if 
+##       it is found.
+## CachSolve will create a get an already computed inverse from cache 
+##      if available. If it is not available, the inverse will be calculated 
+##      and stored in cache.
 
-## Write a short comment describing this function
+
+## This function will return the cached inverse if it exist in cache. If the 
+## does not exist it will return null.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse_value) m <<- inverse_value
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function will attempt to get the inverse value from cache. And write it
+## to cache if needed. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse_value = x$getinverse()
+        if (!is.null(inverse_value)){ ## check if inverse is in cache
+                message("got inverse from cache")
+                return(inverse_value)
+                
+                
+        }
+        
+        ## we did not find the inverse...so it will be calculated and stored.
+        data <- x$get() # create matrix for x
+        inverse_value <- solve(data) # make inverse
+        x$setinverse(inverse_value)
+        inverse_value # return inverse
+        
+        
 }
